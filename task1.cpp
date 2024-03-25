2878105731bf7280435eacff13ad084c210c994e
oksanaemelyanova <159081930+oksanaemelyanova@users.noreply.github.com>
2024-03-25 14:00:48 +0000

    task1

--- task1.cpp
+++ task1.cpp
@@ -7,76 +7,54 @@ class Calculator
     int num2;
 
   Calculator(double num1, double num2) {
-    if (set_num1(num1) == false || set_num2(num2) == false)
-    {
-      std::cout << "Error: Invalid input" << std::endl;
-    }
-    else
-    {
+      set_num1(num1); 
+      set_num2(num2);
       std::cout << "num1 + num2 = " << add() << std::endl;
       std::cout << "num1 - num2 = " << subtract_1_2() << std::endl;
       std::cout << "num2 - num1 = " << subtract_2_1() << std::endl;
       std::cout << "num1 * num2 = " << multiply() << std::endl;
       std::cout << "num1 / num2 = " << divide_1_2() << std::endl;
       std::cout << "num1 / num2 = " << divide_2_1() << std::endl;
-      
-    }
   }
 
-  double add()
+  int add()
   {
     return num1 + num2;  
   }
 
-  double multiply()
+  int multiply()
   {
     return num1 * num2;
   }
 
-  double subtract_1_2()
+  int subtract_1_2()
   {
     return num1 - num2;
   }
 
-  double subtract_2_1()
+  int subtract_2_1()
   {
     return num2 - num1;
   }
 
-  double divide_1_2()
+  int divide_1_2()
   {
     return num1 / num2;
   }
 
-  double divide_2_1()
+  int divide_2_1()
   {
     return num2 / num1;
   }
 
-  bool set_num1(double num1)
+  void set_num1(double num1)
   {
-    if (num1 == 0)
-    {
-      return false;
-    }
-    else
-    {
-      this->num1 = num1;
-      return true;
-    }
+    this->num1 = num1;
   }
 
-  bool set_num2(double num2)
+  void set_num2(double num2)
   {
-    if (num2 == 0)
-    {
-      return false;
-    }
-    else
-    {
-      this->num2 = num2;
-      return true;
-    }
+    this->num2 = num2;
   }
       
 };
@@ -90,6 +68,11 @@ int main() {
   std::cout << "Enter number 2: ";
   std::cin >> num2;
 
+  if (num1 == 0 || num2 == 0)
+  {
+    std::cout << "Error: Invalid input" << std::endl;
+    return(-1);
+  }
   Calculator c1 (num1, num2);
 
   return (0);  
